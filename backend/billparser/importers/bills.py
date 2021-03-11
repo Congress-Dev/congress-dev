869bd1256085270e890e7f513a4ed36467eda65c
Bradley Goodyear <mustyoshi@gmail.com>
2021-03-10 20:31:03 -0500

    Fix issue for bills with only 1 section under the legis-body
    - There was a bug that if there was only one section (Senate-254-IS, House-944-IH, Senate-325-IS, House-504-IH, House-221-IH), it would not properly create objects and recurse

--- backend/billparser/importers/bills.py
+++ backend/billparser/importers/bills.py
@@ -2,7 +2,13 @@ import os
 import sys
 import json
 import zipfile
+import argparse
+
 from billparser.run_through import parse_archive, ensure_congress
+
+parser = argparse.ArgumentParser(description="Reprocess")
+parser.add_argument("--bill", type=str, help="Which bill you want to reprocess")
+
 
 
 def download_path(url: str):
@@ -20,10 +26,21 @@ if __name__ == "__main__":
     will then parse it out and attempt to load each version into
     the database
     """
+    chamber_lookup = {"HR": "House", "S": "Senate"}
     bill_versions = json.load(open(sys.argv[1], "rt"))
+    bill = None
+    if len(sys.argv) > 2:
+        bill = sys.argv[2]
     for rp in bill_versions:
         print("=" * 5)
         print(rp.get("title"))
         zip_file_path = "bills/" + download_path(rp.get("url"))
         ensure_congress(rp.get("congress", 117))
-        parse_archive(zip_file_path)
+        if bill != None:
+            parse_archive(
+                zip_file_path,
+                chamber_filter=chamber_lookup[bill.split(" ")[0]],
+                number_filter=bill.split(" ")[1],
+            )
+        else:
+            parse_archive(zip_file_path)
