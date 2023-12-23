cf9238ca87394249fa723a8265f7210611ce1d68
Bradley Goodyear <mustyoshi@gmail.com>
2023-12-23 15:44:15 -0500

    Update versions and add setup.py
    - Had to update them to get it working on my Win 11 machine
    - Creating the module so that I can iterate on bills in a notebook

---
+++ backend/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='billparser',
+    version='0.1.0',
+    author='Bradley',
+    author_email='mustyoshi@gmail.com',
+    description='Congress.dev bill parser',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/billparser',
+    packages=find_packages(exclude=['tests'], include=["billparser"]),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=[
+        # list of your library dependencies
+    ],
+)
