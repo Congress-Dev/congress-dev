3f66f77357cb062b157e4b5aec6d19490d552b32
Bradley Goodyear <mustyoshi@gmail.com>
2023-12-30 23:33:24 -0500

    Add CORS and exception logging
    - Will improve logging later

--- backend/congress_fastapi/app.py
+++ backend/congress_fastapi/app.py
@@ -1,6 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.responses import Response
+import traceback
+from fastapi.middleware.cors import CORSMiddleware
 from congress_fastapi.routes.members import router as members_router
 
+origins = [
+    "http://localhost:3000",
+    "https://congress.dev",
+]
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specific origins (or ["*"] for all origins)
+    allow_credentials=True,  # Allow cookies to be sent with requests
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+@app.middleware("http")
+async def log_exceptions_middleware(request: Request, call_next):
+    try:
+        # Process the request
+        return await call_next(request)
+    except Exception as e:
+        # Log the exception to the console
+        print(f"Exception occurred: {e}")
+        traceback.print_exc()
+
+        # Re-raise the exception to let FastAPI handle it
+        raise e
+
+
 app.include_router(members_router)
 print("Loaded")
