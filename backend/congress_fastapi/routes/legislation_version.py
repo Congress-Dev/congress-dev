44ca28a95fe070249328601aa7430a4286c85b97
Bradley Goodyear <mustyoshi@gmail.com>
2024-12-24 20:48:46 -0500

    Add content summary table

--- backend/congress_fastapi/routes/legislation_version.py
+++ backend/congress_fastapi/routes/legislation_version.py
@@ -4,9 +4,13 @@ from fastapi import APIRouter, HTTPException, status
 
 from congress_fastapi.handlers.legislation_version import (
     get_legislation_version_tags_by_legislation_id,
+    get_legislation_version_summaries_by_legislation_id,
 )
 from congress_fastapi.models.errors import Error
-from congress_fastapi.models.legislation import LegislationClauseTag
+from congress_fastapi.models.legislation import (
+    LegislationClauseTag,
+    LegislationClauseSummary,
+)
 
 router = APIRouter()
 
@@ -29,6 +33,33 @@ async def get_legislation_version_tags(
 ) -> List[LegislationClauseTag]:
     """Returns a list of LegislationClauseTag objects for a given legislation_id"""
     obj = await get_legislation_version_tags_by_legislation_id(legislation_version_id)
+    if obj is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Legislation not found"
+        )
+    return obj
+
+
+@router.get(
+    "/legislation_version/{legislation_version_id}/summaries",
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "model": Error,
+            "detail": "Legislation not found",
+        },
+        status.HTTP_200_OK: {
+            "model": List[LegislationClauseTag],
+            "detail": "Summaries for the sections in the legislation",
+        },
+    },
+)
+async def get_legislation_version_summaries(
+    legislation_version_id: int,
+) -> List[LegislationClauseSummary]:
+    """Returns a list of LegislationClauseSummary objects for a given legislation_id"""
+    obj = await get_legislation_version_summaries_by_legislation_id(
+        legislation_version_id
+    )
     if obj is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Legislation not found"
