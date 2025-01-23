f3c1242a44a217dbb5a20918af9c91a276637074
Bradley Goodyear <mustyoshi@gmail.com>
2025-01-23 18:41:41 -0500

    Allow passing a usc_relrease url to the release importer

--- backend/billparser/importers/releases.py
+++ backend/billparser/importers/releases.py
@@ -1,31 +1,45 @@
-
-from datetime import datetime
-from joblib import Parallel, delayed
-from lxml import html
+import argparse
+import html
 import os
-import re
+import zipfile
+from datetime import datetime
+from billparser.importers.bills import download_path
+from joblib import Parallel, delayed
 import requests
 from sqlalchemy import func
-import zipfile
-
 from billparser.db.handler import import_title, get_number, Session
 from billparser.db.models import USCRelease, Version
-
 
 THREADS = int(os.environ.get("PARSE_THREADS", -1))
 DOWNLOAD_BASE = "https://uscode.house.gov/download/{}"
 RELEASE_POINTS = "https://uscode.house.gov/download/priorreleasepoints.htm"
 
-def download_path(url: str):
-    os.makedirs("usc", exist_ok=True)
-    output_name = url.split("@")[-1]
-    if os.path.exists(f"usc/{output_name}"):
-        return output_name
-    os.system(f"wget {url} --output-document usc/{output_name}")
-    return output_name
+def main():
+    parser = argparse.ArgumentParser(description="Process release points.")
+    parser.add_argument('--release-point', type=str, help='URL of the zip file to process a single release point')
+    args = parser.parse_args()
 
+    if args.release_point:
+        process_single_release_point(args.release_point)
+    else:
+        process_all_release_points()
 
-if __name__ == "__main__":
+def process_single_release_point(url, release = None):
+    zip_file_path = download_path(url)
+    with zipfile.ZipFile(zip_file_path) as zip_file:
+        files = zip_file.namelist()
+        files = sorted(files, key=lambda x: get_number(x.split(".")[0].replace("usc", "")))
+        Parallel(n_jobs=THREADS, verbose=5, backend="loky")(
+            delayed(import_title)(
+                zip_file.open(file).read(),
+                file.split(".")[0].replace("usc", ""),
+                None,  # Assuming title is not needed for single release point
+                release   # Assuming release_point.to_dict() is not needed for single release point
+            )
+            for file in files
+        )
+
+def process_all_release_points():
     release_points = []
     response = requests.get(RELEASE_POINTS)
     tree = html.fromstring(response.content)
@@ -87,4 +101,7 @@ if __name__ == "__main__":
                 )
                 for file in files  # if "09" in file
             )
+
+if __name__ == "__main__":
+    main()
 
