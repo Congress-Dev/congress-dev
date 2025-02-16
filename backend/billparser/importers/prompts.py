680062f4f919e0a2c3ce05bb920258cc209611f6
Bradley Goodyear <mustyoshi@gmail.com>
2025-02-16 13:02:19 -0500

    ree

--- backend/billparser/importers/prompts.py
+++ backend/billparser/importers/prompts.py
@@ -64,8 +64,9 @@ if __name__ == "__main__":
             selected_prompt = prompt_list[0]
             prompt_id = selected_prompt.prompt_id
             # Reverse it so we do newer bills first
-            results = get_outstanding_legis_versions([l.prompt_id for l in prompt_list])[::-1]
+            results = get_outstanding_legis_versions([l.prompt_id for l in prompt_list])
             results.sort()
+            results = results[::-1]
             logging.info(
                 f"Found {len(results)} legislation versions without prompt batch for {prompt_name}"
             )
