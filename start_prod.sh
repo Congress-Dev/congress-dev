54a11a464020a52e6a8a147ce6575392e4a243e3
Bradley Goodyear <mustyoshi@gmail.com>
2025-05-04 21:45:13 -0400

    Force rebuild

--- start_prod.sh
+++ start_prod.sh
@@ -1,1 +1,3 @@
+docker-compose -f .docker/docker-compose.yml -f .docker/docker-compose.prod.yml build
+docker-compose -f .docker/docker-compose.yml -f .docker/docker-compose.prod.yml down
 docker-compose -f .docker/docker-compose.yml -f .docker/docker-compose.prod.yml up -d
