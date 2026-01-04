7dd5ba9601f9f654188c8cd1cfb9447a46d76942
Brooks Ryba <brooksryba@gmail.com>
2026-01-03 22:01:27 -0500

    Fix overview legislation version date bug

--- hillstack/src/app/congress/bills/[billId]/overview/page.tsx
+++ hillstack/src/app/congress/bills/[billId]/overview/page.tsx
@@ -50,7 +50,7 @@ export default async function BillOverviewPage({
 					>
 						{data?.legislation_version?.map((version) => (
 							<TimelineNode
-								date={version.effective_date ?? new Date()}
+								date={version.effective_date ?? version.created_at ?? new Date()}
 								icon={ReadMoreIcon}
 								key={version.legislation_version}
 								title={
